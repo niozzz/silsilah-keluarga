a3dc140e0680f70d1e577e6c9f6c89a34d9fc7a3
niozzz <rachmatjackson@gmail.com>
2022-06-19 09:17:09 +0700

    menambahkan tampilan tombol hapus

--- public/input-keluarga.php
+++ public/input-keluarga.php
@@ -2,6 +2,9 @@
 
 
 require "config.php";
+
+$keluarga = query("SELECT * FROM keluarga");
+
 if (isset($_POST['submit']))
 {
     $namaKeluarga = htmlspecialchars($_POST['namaKeluarga']);
@@ -37,7 +40,7 @@ if (isset($_POST['submit']))
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -66,111 +69,132 @@ if (isset($_POST['submit']))
     <!-- ===============================================-->
     <link href="assets/css/theme.css" rel="stylesheet" />
 
-  </head>
+</head>
 
 
-  <body>
+<body>
 
     <!-- ===============================================-->
     <!--    Main Content-->
     <!-- ===============================================-->
     <main class="main" id="top">
-      <!-- Navbar -->
+        <!-- Navbar -->
 
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
-        <div class="container"><a class="navbar-brand" href="#"><img src="assets/img/gallery/logo.png" width="118" alt="logo" /></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
-          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
-              
-              <li class="nav-item px-2"><a class="nav-link" href="/silsilah/public">Home</a></li>
-            </ul><a class="btn btn-sm btn-outline-primary rounded-pill order-1 order-lg-0 ms-lg-4" href="/silsilah/public/login.php">Login</a>
-          </div>
-        </div>
-      </nav>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block"
+            data-navbar-on-scroll="data-navbar-on-scroll">
+            <div class="container"><a class="navbar-brand" href="#"><img src="assets/img/gallery/logo.png" width="118"
+                        alt="logo" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon">
+                    </span></button>
+                <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
 
-      <!-- end Navbar -->
-      <!-- ============================================-->
-      <!-- <section> begin ============================-->
-        
+                        <li class="nav-item px-2"><a class="nav-link" href="/silsilah/public">Home</a></li>
+                    </ul><a class="btn btn-sm btn-outline-primary rounded-pill order-1 order-lg-0 ms-lg-4"
+                        href="/silsilah/public/login.php">Login</a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- end Navbar -->
+        <!-- ============================================-->
+        <!-- <section> begin ============================-->
+
         <!-- <section> close ============================-->
         <!-- ============================================-->
-  
-  
+
+
         <section class="py-8">
-          <div class="container">
-            <div class="row">
-              <div class="bg-holder bg-size" style="background-image:url(assets/img/gallery/dot-bg.png);background-position:bottom right;background-size:auto;">
-              </div>
-              <!--/.bg-holder-->
-  
-              <div class="col-lg-6 z-index-2 mb-5"><img class="w-100" src="assets/img/gallery/appointment.png" alt="..." /></div>
-              <div class="col-lg-6 z-index-2">
-                <h1 class="fw-light font-base fs-6 fs-xxl-7">Input <strong>Nama Keluarga</strong></h1>
-                <form class="row g-3" action="" method="POST">
-                  
-                  <div class="col-md-12">
-                    <label class="form-label visually-hidden" for="namaKeluarga">Nama Keluarga</label>
-                    <input class="form-control form-livedoc-control" id="namaKeluarga" name="namaKeluarga" type="text" placeholder="Nama Keluarga (Keturunan Mbah Yutir)" required />
-                  </div>
-                  <div class="col-md-12">
-                    <label class="form-label visually-hidden" for="namaPasangan">Nama Pasangan</label>
-                    <input class="form-control form-livedoc-control" id="namaPasangan" name="namaPasangan" type="text" placeholder="Nama Pasangan (optional)" />
-                  </div>
-                  <div class="col-md-12">
-                    <label class="form-label visually-hidden" for="domisili">Domisili</label>
-                    <!-- <textarea class="form-control form-livedoc-control" id="domisili" name="domisili" type="text" placeholder="Domisili"  required > </textarea> -->
-                    <textarea  class="form-control form-livedoc-control" id="domisili" name="domisili"  placeholder="Domisili"></textarea>
-                  </div>
-                  
-                  
-                  <div class="col-12">
-                    <div class="d-grid">
-                      <button class="btn btn-primary rounded-pill" name="submit" type="submit">Submit</button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 z-index-2">
+                        <h1 class="fw-light font-base fs-6 fs-xxl-7">Seluruh <strong>Anggota Keluarga </strong></h1>
+
+                        <ol class="list-group list-group-numbered">
+                            <?php foreach($keluarga as $d) : ?>
+                            <li class="list-group-item"><?= $d['nama_keluarga'] ?><a href="#"><span
+                                        class="badge rounded-pill bg-danger float-end"> Hapus </span> </a></li>
+                            <?php endforeach; ?>
+
+                        </ol>
+
                     </div>
-                  </div>
-                </form>
-              </div>
+                    <div class="col-lg-6 z-index-2">
+                        <h1 class="fw-light font-base fs-6 fs-xxl-7">Input <strong>Nama Keluarga</strong></h1>
+                        <form class="row g-3" action="" method="POST">
+
+                            <div class="col-md-12">
+                                <label class="form-label visually-hidden" for="namaKeluarga">Nama Keluarga</label>
+                                <input class="form-control form-livedoc-control" id="namaKeluarga" name="namaKeluarga"
+                                    type="text" placeholder="Nama Keluarga (Keturunan Mbah Yutir)" required />
+                            </div>
+                            <div class="col-md-12">
+                                <label class="form-label visually-hidden" for="namaPasangan">Nama Pasangan</label>
+                                <input class="form-control form-livedoc-control" id="namaPasangan" name="namaPasangan"
+                                    type="text" placeholder="Nama Pasangan (optional)" />
+                            </div>
+                            <div class="col-md-12">
+                                <label class="form-label visually-hidden" for="domisili">Domisili</label>
+                                <!-- <textarea class="form-control form-livedoc-control" id="domisili" name="domisili" type="text" placeholder="Domisili"  required > </textarea> -->
+                                <textarea class="form-control form-livedoc-control" id="domisili" name="domisili"
+                                    placeholder="Domisili"></textarea>
+                            </div>
+
+
+                            <div class="col-12">
+                                <div class="d-grid">
+                                    <button class="btn btn-primary rounded-pill" name="submit"
+                                        type="submit">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
         </section>
 
-      
 
-
-      <!-- ============================================-->
-      <!-- <section> begin ============================-->
-      
-      <section class="py-0 bg-secondary">
-        
 
 
         <!-- ============================================-->
         <!-- <section> begin ============================-->
-        <section class="py-0 bg-primary">
 
-          <div class="container">
-            <div class="row justify-content-md-between justify-content-evenly py-4">
-              <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
-                <p class="fs--1 my-2 fw-bold text-200">All rights Reserved &copy; Your Company, 2021</p>
-              </div>
-              <div class="col-12 col-sm-8 col-md-6">
-                <p class="fs--1 my-2 text-center text-md-end text-200"> Made with&nbsp;
-                  <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#F95C19" viewBox="0 0 16 16">
-                    <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
-                  </svg>&nbsp;by&nbsp;<a class="fw-bold text-info" href="https://themewagon.com/" target="_blank">ThemeWagon </a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <!-- end of .container-->
+        <section class="py-0 bg-secondary">
+
+
+
+            <!-- ============================================-->
+            <!-- <section> begin ============================-->
+            <section class="py-0 bg-primary">
+
+                <div class="container">
+                    <div class="row justify-content-md-between justify-content-evenly py-4">
+                        <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
+                            <p class="fs--1 my-2 fw-bold text-200">All rights Reserved &copy; Your Company, 2021</p>
+                        </div>
+                        <div class="col-12 col-sm-8 col-md-6">
+                            <p class="fs--1 my-2 text-center text-md-end text-200"> Made with&nbsp;
+                                <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="12"
+                                    height="12" fill="#F95C19" viewBox="0 0 16 16">
+                                    <path
+                                        d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z">
+                                    </path>
+                                </svg>&nbsp;by&nbsp;<a class="fw-bold text-info" href="https://themewagon.com/"
+                                    target="_blank">ThemeWagon </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of .container-->
+
+            </section>
+            <!-- <section> close ============================-->
+            <!-- ============================================-->
+
 
         </section>
-        <!-- <section> close ============================-->
-        <!-- ============================================-->
-
-
-      </section>
     </main>
     <!-- ===============================================-->
     <!--    End of Main Content-->
@@ -192,7 +216,9 @@ if (isset($_POST['submit']))
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&amp;family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100&amp;display=swap" rel="stylesheet">
-  </body>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Fjalla+One&amp;family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100&amp;display=swap"
+        rel="stylesheet">
+</body>
 
 </html>
